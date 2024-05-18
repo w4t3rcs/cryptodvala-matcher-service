80a279cd3805b3f8776958f99b521324059148a5
w4t3rcs <uimanovmaks@gmail.com>
2024-05-18 21:19:15 +0200

    Initial commit

---
+++ src/main/java/com/cryptodvala/matcher/MatcherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cryptodvala.matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvalaPatternMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvalaPatternMatcherApplication.class, args);
+    }
+
+}
